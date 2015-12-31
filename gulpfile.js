874f1b13bfdd0fe827129b901c2ec3bbee1e77a6
oking02 <thorax123>
2015-12-31 16:16:27 +0000

    Finished the main structure, Footer and the Home page.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,19 @@
  * Created by ollyking on 30/12/2015.
  */
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function(){
     gulp.src('css/**/*.css')
         .pipe(concat('style.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch-css', function(){
+    gulp.watch('css/**/*.css', ['css'])
 });
